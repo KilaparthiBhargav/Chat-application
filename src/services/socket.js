b3bf12e55e2fb063adf0ca0c1ff6f9aa44228714
KilaparthiBhargav <bhargav908716@gmail.com>
2026-05-24 14:04:25 +0530

    added backed socket connection

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+// export const socket = io("http://localhost:3001");
+const socket = io("https://chat-application-ae75.onrender.com");
